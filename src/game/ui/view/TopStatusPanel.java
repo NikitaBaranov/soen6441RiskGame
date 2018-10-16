4fc798372be99682f5d90f18870dcf3ce17ea16f
Dmitry Kryukov <remasik@gmail.com>
2018-10-16 14:23:52 -0400

    Fix color changing in status panel

--- src/game/ui/view/TopStatusPanel.java
+++ src/game/ui/view/TopStatusPanel.java
@@ -11,40 +11,40 @@ public class TopStatusPanel extends JPanel {
     private JLabel gameState = new JLabel();
     private JLabel gamePhase = new JLabel();
     private JLabel turnPhrase = new JLabel();
-    private int pColor = 1213123; // default color
 
     public TopStatusPanel(int width, int height) {
         this.setPreferredSize(new Dimension(width, height));
         this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 
-        this.add(new JLabel("Player: "));
+        this.add(new JLabel(" Player: "));
         this.add(playerName);
-        playerName.setForeground(new Color(pColor)); // I cant update the color for each player
-
         JSeparator separator1 = new JSeparator(SwingConstants.HORIZONTAL); // Horizontal separator
         separator1.setBackground(new Color(121,180,115)); // make it invisible
         separator1.setForeground(new Color(121,180,115)); // make it invisible
         this.add(separator1);
+
         this.add(new JLabel("State: "));
         this.add(gameState);
         JSeparator separator2 = new JSeparator(SwingConstants.HORIZONTAL); // Horizontal separator
         separator2.setBackground(new Color(121,180,115)); // make it invisible
         separator2.setForeground(new Color(121,180,115)); // make it invisible
         this.add(separator2);
+
         this.add(new JLabel("Status: "));
         this.add(turnPhrase);
         JSeparator separator3 = new JSeparator(SwingConstants.HORIZONTAL); // Horizontal separator
         separator3.setBackground(new Color(121,180,115)); // make it invisible
         separator3.setForeground(new Color(121,180,115)); // make it invisible
         this.add(separator3);
+
         this.add(new JLabel("Phase: "));
         this.add(gamePhase);
     }
 
     public void setPlayer(Player player) {
         playerName.setText(player.getName());
-        String colorS = Integer.toString(player.getColor().getRGB()); // get color for player
-        pColor = Integer.parseInt(colorS); // set color var
+        String colorS = Integer.toString(player.getColor().getRGB());
+        playerName.setForeground(new Color(Integer.parseInt(colorS)));
     }
 
     public void setGameState(String gameState) {
