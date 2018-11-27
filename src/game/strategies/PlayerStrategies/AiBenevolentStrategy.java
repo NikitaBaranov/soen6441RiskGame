3d88f8a46cf9e1ac93594532e386c1761c11db50
Dmitry Kryukov <remasik@gmail.com>
2018-11-27 10:49:00 -0500

    Message about saving a game

--- src/game/strategies/PlayerStrategies/AiBenevolentStrategy.java
+++ src/game/strategies/PlayerStrategies/AiBenevolentStrategy.java
@@ -1,13 +1,20 @@
 package game.strategies.PlayerStrategies;
 
+import static game.strategies.GamePhaseStrategies.BasePhaseStrategy.isGameWonBy;
+import static game.strategies.GamePhaseStrategies.GamePhaseEnum.GAME_OVER;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.swing.SwingWorker;
+
+import game.Game;
 import game.model.Country;
 import game.model.GameState;
 import game.model.enums.CardsEnum;
+import game.strategies.GamePhaseStrategies.GamePhaseStrategyFactory;
 
 public class AiBenevolentStrategy extends BasePlayerStrategy {
 	
@@ -17,65 +24,8 @@ public class AiBenevolentStrategy extends BasePlayerStrategy {
      */
     @Override
     public void placeArmies(GameState gameState) {
-    	System.out.println("Bene place armies");
-    	HashMap<Integer, ArrayList<Country>> armyOnCountries = new HashMap<Integer, ArrayList<Country>>();
-
-    	for (Country country : gameState.getCountries()) {
-            if (country.getPlayer() == gameState.getCurrentPlayer()) {
-            	if(armyOnCountries.containsKey(country.getArmy())) {
-            		ArrayList<Country> tmp = armyOnCountries.get(country.getArmy());
-            		tmp.add(country);
-            	}
-            	else {
-            		ArrayList<Country> tmp = new ArrayList<Country>();
-            		tmp.add(country);
-            		armyOnCountries.put(country.getArmy(), tmp);
-            	}
-            }
-        }
-    	int playerArmies = gameState.getCurrentPlayer().getArmies();
-    	
-    	for(int i = 0, j = 0; i < playerArmies; j++)
-    	{
-    		ArrayList<Country> tmpCountries = armyOnCountries.get(j);
-    		if(tmpCountries == null) continue;
-    		for(int k = 0; k < tmpCountries.size(); k++) {
-    			Country cntry = tmpCountries.remove(k);
-    			cntry.setArmy(cntry.getArmy() + 1);
-    			if(armyOnCountries.containsKey(cntry.getArmy())) {
-    				armyOnCountries.get(cntry.getArmy()).add(cntry);
-    			}
-    			else {
-    				ArrayList<Country> tC = new ArrayList<Country>();
-    				tC.add(cntry);
-    				armyOnCountries.put(cntry.getArmy(), tC);
-    			}
-    			i++;
-    		}
-    	}
-    	gameState.getCurrentPlayer().setArmies(0);
-    }
-
-    /**
-     * method needs to be implemented
-     * @param gameState
-     */
-    @Override
-    public void exchange(GameState gameState) {
-    	System.out.println("Bene exchange");
-    	String phrase = "";
-        if (gameState.getSelectedCardsToExchange().size() == 3) {
-            for (CardsEnum cardsEnum : gameState.getSelectedCardsToExchange()) {
-                gameState.getCurrentPlayer().getCardsEnumIntegerMap().put(cardsEnum, gameState.getCurrentPlayer().getCardsEnumIntegerMap().get(cardsEnum) - 1);
-            }
-            phrase = String.join(", ", gameState.getSelectedCardsToExchange().stream().map(CardsEnum::getName).collect(Collectors.toList())) + " cards";
-        } else if (gameState.getSelectedCardsToExchange().size() == 1) {
-            gameState.getCurrentPlayer().getCardsEnumIntegerMap().put(gameState.getSelectedCardsToExchange().get(0), gameState.getCurrentPlayer().getCardsEnumIntegerMap().get(gameState.getSelectedCardsToExchange().get(0)) - 3);
-            phrase = gameState.getSelectedCardsToExchange().get(0).getName() + " card";
-        }
-        gameState.getCurrentPlayer().setArmies(gameState.getCurrentPlayer().getArmies() + gameState.getArmiesToCardExchange());
-        gameState.setArmiesToCardExchange(gameState.getArmiesToCardExchange() + gameState.getARMIES_TO_EXCHANGE_INCREASE());
-        gameState.setCurrentTurnPhraseText("Exchanged " + phrase + " for " + gameState.getARMIES_TO_EXCHANGE_INCREASE() + " armies. Armies to place " + gameState.getCurrentPlayer().getArmies());
+    	System.out.println("AI Benevolent Place Armies!");
+        new PlaceArmiesWorker(gameState).execute();
     }
 
     /**
@@ -84,8 +34,10 @@ public class AiBenevolentStrategy extends BasePlayerStrategy {
      */
     @Override
     public void reinforce(GameState gameState) {
-    	System.out.println("Bene reinforces");
-    	placeArmies(gameState);
+    	exchangeCards(gameState);
+        pauseAndRefresh(gameState, PAUSE * 2);
+    	System.out.println("AI Benevolent Reinforce!");
+        new PlaceArmiesWorker(gameState).execute();
         //System.out.println("Reinforcement is not implemented in " + this.getClass().getName() + " strategy.");
     }
 
@@ -105,7 +57,7 @@ public class AiBenevolentStrategy extends BasePlayerStrategy {
     @Override
     public void attack(GameState gameState) {
     	System.out.println("Bene attacks");
-        return;
+    	new AttackWorker(gameState).execute();
     }
 
     /**
@@ -114,55 +66,304 @@ public class AiBenevolentStrategy extends BasePlayerStrategy {
      */
     @Override
     public void fortify(GameState gameState) {
-    	System.out.println("Bene fortifies");
-    	HashMap<Integer, ArrayList<Country>> armyOnCountries = new HashMap<Integer, ArrayList<Country>>();
-    	int max = 0;
-    	Country maxArmyCountry = null;
-    	
-    	for (Country country : gameState.getCountries()) {
-            if (country.getPlayer() == gameState.getCurrentPlayer()) {
-            	if(armyOnCountries.containsKey(country.getArmy())) {
-            		ArrayList<Country> tmp = armyOnCountries.get(country.getArmy());
-            		tmp.add(country);
-            	}
-            	else {
-            		ArrayList<Country> tmp = new ArrayList<Country>();
-            		tmp.add(country);
-            		armyOnCountries.put(country.getArmy(), tmp);
-            	}
-            	if(country.getArmy() > max) {
-            		List<Country> neighbours = country.getNeighbours();
-            		for(int ctrN = 0; ctrN < neighbours.size(); ctrN++) {
-            			if(neighbours.get(ctrN).getPlayer() == gameState.getCurrentPlayer()) {
-                    		maxArmyCountry = country;
-                    		max = country.getArmy();
-            			}
-            		}
-            	}
-            }
-        }
-    	
-    	if(maxArmyCountry != null) {
-    		int min = 100;
-    		Country minCountry = null;
-    		List<Country> neighbours = maxArmyCountry.getNeighbours();
-    		for(int ctrN = 0; ctrN < neighbours.size(); ctrN++) {
-    			if(neighbours.get(ctrN).getPlayer() == gameState.getCurrentPlayer()) {
-            		if(neighbours.get(ctrN).getArmy() < min) {
-            			min = neighbours.get(ctrN).getArmy();
-            			minCountry = neighbours.get(ctrN);
-            		}
-    			}
-    		}
-    		
-    		if(minCountry != null) {
-	    		while(minCountry.getArmy() <= maxArmyCountry.getArmy()) {
-	    			minCountry.setArmy(minCountry.getArmy() + 1);
-	    			maxArmyCountry.setArmy(maxArmyCountry.getArmy() + 1);
-	    		}
-    		}
-    	}
+    	System.out.println("AI Benevolent Fortify!");
+        new FortifyWorker(gameState).execute();
         //System.out.println("Fortifying is not implemented in " + this.getClass().getName() + " strategy.");
     }
+    
 
+    private class PlaceArmiesWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class.
+         *
+         * @param gameState
+         */
+        public PlaceArmiesWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Automatic reinforcement in backgrounds.
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+        	System.out.println("Bene place armies");
+        	HashMap<Integer, ArrayList<Country>> armyOnCountries = new HashMap<Integer, ArrayList<Country>>();
+
+        	for (Country country : gameState.getCountries()) {
+                if (country.getPlayer() == gameState.getCurrentPlayer()) {
+                	if(armyOnCountries.containsKey(country.getArmy())) {
+                		ArrayList<Country> tmp = armyOnCountries.get(country.getArmy());
+                		tmp.add(country);
+                	}
+                	else {
+                		ArrayList<Country> tmp = new ArrayList<Country>();
+                		tmp.add(country);
+                		armyOnCountries.put(country.getArmy(), tmp);
+                	}
+                }
+            }
+        	int playerArmies = gameState.getCurrentPlayer().getArmies();
+        	
+        	for(int i = 0, j = 0; i < playerArmies; j++)
+        	{
+        		ArrayList<Country> tmpCountries = armyOnCountries.get(j);
+        		if(tmpCountries == null) continue;
+        		for(int k = 0; k < tmpCountries.size(); k++) {
+        			Country cntry = tmpCountries.remove(k);
+        			cntry.setArmy(cntry.getArmy() + 1);
+        			if(armyOnCountries.containsKey(cntry.getArmy())) {
+        				armyOnCountries.get(cntry.getArmy()).add(cntry);
+        			}
+        			else {
+        				ArrayList<Country> tC = new ArrayList<Country>();
+        				tC.add(cntry);
+        				armyOnCountries.put(cntry.getArmy(), tC);
+        			}
+        			i++;
+        		}
+        	}
+        	gameState.getCurrentPlayer().setArmies(0);
+
+            pauseAndRefresh(gameState, PAUSE);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+        }
+    }
+
+    /**
+     * Worker for reinforcement phase.
+     * required to correctly repaint swing components.
+     */
+    private class ReinforceWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class.
+         *
+         * @param gameState
+         */
+        public ReinforceWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Automatic reinforcement in backgrounds.
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+            /*
+
+            You need to put your code here
+
+             */
+
+            pauseAndRefresh(gameState, PAUSE);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+        }
+    }
+
+    /**
+     * Fortifying phase worker for AI.
+     * Required for correctly repaint the swing components.
+     */
+    private class FortifyWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class
+         *
+         * @param gameState
+         */
+        public FortifyWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Do the foctification actions in the background
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+        	System.out.println("Bene fortifies");
+        	HashMap<Integer, ArrayList<Country>> armyOnCountries = new HashMap<Integer, ArrayList<Country>>();
+        	int max = 0;
+        	Country maxArmyCountry = null;
+        	
+        	for (Country country : gameState.getCountries()) {
+                if (country.getPlayer() == gameState.getCurrentPlayer()) {
+                	if(armyOnCountries.containsKey(country.getArmy())) {
+                		ArrayList<Country> tmp = armyOnCountries.get(country.getArmy());
+                		tmp.add(country);
+                	}
+                	else {
+                		ArrayList<Country> tmp = new ArrayList<Country>();
+                		tmp.add(country);
+                		armyOnCountries.put(country.getArmy(), tmp);
+                	}
+                	if(country.getArmy() > max) {
+                		List<Country> neighbours = country.getNeighbours();
+                		for(int ctrN = 0; ctrN < neighbours.size(); ctrN++) {
+                			if(neighbours.get(ctrN).getPlayer() == gameState.getCurrentPlayer()) {
+                        		maxArmyCountry = country;
+                        		max = country.getArmy();
+                			}
+                		}
+                	}
+                }
+            }
+        	
+        	if(maxArmyCountry != null) {
+        		int min = 100;
+        		Country minCountry = null;
+        		List<Country> neighbours = maxArmyCountry.getNeighbours();
+        		for(int ctrN = 0; ctrN < neighbours.size(); ctrN++) {
+        			if(neighbours.get(ctrN).getPlayer() == gameState.getCurrentPlayer()) {
+                		if(neighbours.get(ctrN).getArmy() < min) {
+                			min = neighbours.get(ctrN).getArmy();
+                			minCountry = neighbours.get(ctrN);
+                		}
+        			}
+        		}
+        		
+        		if(minCountry != null) {
+    	    		while(minCountry.getArmy() <= maxArmyCountry.getArmy()) {
+    	    			minCountry.setArmy(minCountry.getArmy() + 1);
+    	    			maxArmyCountry.setArmy(maxArmyCountry.getArmy() + 1);
+    	    		}
+        		}
+        	}
+
+            pauseAndRefresh(gameState, PAUSE * 2);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+        }
+    }
+    
+
+    /**
+     * Attack phase worker for Ai
+     * Required to correctly repaint swing components
+     */
+    private class AttackWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class
+         *
+         * @param gameState
+         */
+        public AttackWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Do attack actions in the background.
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+            // No code
+
+            pauseAndRefresh(gameState, PAUSE * 2);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            if (isGameWonBy(gameState, gameState.getCurrentPlayer())) {
+                // TODO Add message that attacker win battle
+                Game.getInstance().setGamePhaseStrategy(GamePhaseStrategyFactory.getStrategy(GAME_OVER));
+                Game.getInstance().getGamePhaseStrategy().init(gameState);
+            } else {
+                Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+            }
+
+        }
+    }
 }
--- src/game/strategies/PlayerStrategies/AiCheaterStrategy.java
+++ src/game/strategies/PlayerStrategies/AiCheaterStrategy.java
@@ -1,10 +1,17 @@
 package game.strategies.PlayerStrategies;
 
+import static game.strategies.GamePhaseStrategies.BasePhaseStrategy.isGameWonBy;
+import static game.strategies.GamePhaseStrategies.GamePhaseEnum.GAME_OVER;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.SwingWorker;
+
+import game.Game;
 import game.model.Country;
 import game.model.GameState;
+import game.strategies.GamePhaseStrategies.GamePhaseStrategyFactory;
 
 public class AiCheaterStrategy extends BasePlayerStrategy {
 
@@ -14,23 +21,8 @@ public class AiCheaterStrategy extends BasePlayerStrategy {
      */
     @Override
     public void placeArmies(GameState gameState) {
-    	System.out.println("Cheater place armies");
-    	for (Country country : gameState.getCountries()) {
-    		if(country.getPlayer() == gameState.getCurrentPlayer()) {
-    			country.setArmy(country.getArmy() * 2);
-    			//System.out.println("Reinforcement: Armies doubled on " + country.getName());
-    		}
-    	}
-    	gameState.getCurrentPlayer().setArmies(0);
-    }
-
-    /**
-     * method needs to be implemented
-     * @param gameState
-     */
-    @Override
-    public void exchange(GameState gameState) {
-        //System.out.println("Exchange is not implemented in " + this.getClass().getName() + " strategy.");
+    	System.out.println("AI Aggressive Place Armies!");
+        new PlaceArmiesWorker(gameState).execute();
     }
 
     /**
@@ -39,14 +31,10 @@ public class AiCheaterStrategy extends BasePlayerStrategy {
      */
     @Override
     public void reinforce(GameState gameState) {
-    	System.out.println("Cheater reinforces");
-    	for (Country country : gameState.getCountries()) {
-    		if(country.getPlayer() == gameState.getCurrentPlayer()) {
-    			country.setArmy(country.getArmy() * 2);
-    			System.out.println("Reinforcement: Armies doubled on " + country.getName());
-    		}
-    	}
-    	gameState.getCurrentPlayer().setArmies(0);
+    	exchangeCards(gameState);
+        pauseAndRefresh(gameState, PAUSE * 2);
+        System.out.println("AI Aggressive Reinforce!");
+        new ReinforceWorker(gameState).execute();
     }
 
     /**
@@ -64,23 +52,8 @@ public class AiCheaterStrategy extends BasePlayerStrategy {
      */
     @Override
     public void attack(GameState gameState) {
-    	System.out.println("Cheater attacks");
-    	ArrayList<Country> countriesCaptured = new ArrayList<Country>();
-    	for (Country country : gameState.getCountries()) {
-    		if(country.getPlayer() == gameState.getCurrentPlayer()) {
-    			List<Country> neighbours = country.getNeighbours();
-    			for(Country neighbour : neighbours) {
-    				if(neighbour.getPlayer() != gameState.getCurrentPlayer() && countriesCaptured.contains(neighbour) == false)
-    					countriesCaptured.add(neighbour);
-    			}
-    		}
-    	}
-    	
-    	System.out.println("Countries to capture: " + countriesCaptured.size());
-    	for (Country country : countriesCaptured) {
-    		country.setPlayer(gameState.getCurrentPlayer());
-    	}
-    	return;
+    	System.out.println("AI Aggressive Attack!");
+        new AttackWorker(gameState).execute();
     }
 
     /**
@@ -89,17 +62,261 @@ public class AiCheaterStrategy extends BasePlayerStrategy {
      */
     @Override
     public void fortify(GameState gameState) {
-    	System.out.println("Cheater fortifies");
-    	for (Country country : gameState.getCountries()) {
-    		if(country.getPlayer() == gameState.getCurrentPlayer()) {
-    			List<Country> neighbours = country.getNeighbours();
-    			for(Country neighbour : neighbours) {
-    				if(neighbour.getPlayer() != gameState.getCurrentPlayer()) {
-    					country.setArmy(country.getArmy() * 2);
-    					//System.out.println("Fortify: Armies doubled on " + country.getName());
-    				}
-    			}
-    		}
-    	}
+    	System.out.println("AI Aggressive Fortify!");
+        new FortifyWorker(gameState).execute();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+
+    private class PlaceArmiesWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class.
+         *
+         * @param gameState
+         */
+        public PlaceArmiesWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Automatic reinforcement in backgrounds.
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+        	System.out.println("Cheater place armies");
+        	for (Country country : gameState.getCountries()) {
+        		if(country.getPlayer() == gameState.getCurrentPlayer()) {
+        			country.setArmy(country.getArmy() * 2);
+        			//System.out.println("Reinforcement: Armies doubled on " + country.getName());
+        		}
+        	}
+        	gameState.getCurrentPlayer().setArmies(0);
+            pauseAndRefresh(gameState, PAUSE);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+        }
+    }
+
+    /**
+     * Worker for reinforcement phase.
+     * required to correctly repaint swing components.
+     */
+    private class ReinforceWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class.
+         *
+         * @param gameState
+         */
+        public ReinforceWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Automatic reinforcement in backgrounds.
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+        	System.out.println("Cheater reinforces");
+        	for (Country country : gameState.getCountries()) {
+        		if(country.getPlayer() == gameState.getCurrentPlayer()) {
+        			country.setArmy(country.getArmy() * 2);
+        			System.out.println("Reinforcement: Armies doubled on " + country.getName());
+        		}
+        	}
+        	gameState.getCurrentPlayer().setArmies(0);
+
+            pauseAndRefresh(gameState, PAUSE);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+        }
+    }
+
+    /**
+     * Attack phase worker for Ai
+     * Required to correctly repaint swing components
+     */
+    private class AttackWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class
+         *
+         * @param gameState
+         */
+        public AttackWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Do attack actions in the background.
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+        	System.out.println("Cheater attacks");
+        	ArrayList<Country> countriesCaptured = new ArrayList<Country>();
+        	for (Country country : gameState.getCountries()) {
+        		if(country.getPlayer() == gameState.getCurrentPlayer()) {
+        			List<Country> neighbours = country.getNeighbours();
+        			for(Country neighbour : neighbours) {
+        				if(neighbour.getPlayer() != gameState.getCurrentPlayer() && countriesCaptured.contains(neighbour) == false)
+        					countriesCaptured.add(neighbour);
+        			}
+        		}
+        	}
+        	
+        	System.out.println("Countries to capture: " + countriesCaptured.size());
+        	for (Country country : countriesCaptured) {
+        		country.setPlayer(gameState.getCurrentPlayer());
+        	}
+
+            pauseAndRefresh(gameState, PAUSE * 2);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            if (isGameWonBy(gameState, gameState.getCurrentPlayer())) {
+                // TODO Add message that attacker win battle
+                Game.getInstance().setGamePhaseStrategy(GamePhaseStrategyFactory.getStrategy(GAME_OVER));
+                Game.getInstance().getGamePhaseStrategy().init(gameState);
+            } else {
+                Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+            }
+
+        }
+    }
+
+    /**
+     * Fortifying phase worker for AI.
+     * Required for correctly repaint the swing components.
+     */
+    private class FortifyWorker extends SwingWorker<Void, String> {
+
+        GameState gameState;
+
+        /**
+         * Constructor of the class
+         *
+         * @param gameState
+         */
+        public FortifyWorker(GameState gameState) {
+            this.gameState = gameState;
+        }
+
+        /**
+         * Do the foctification actions in the background
+         *
+         * @return
+         */
+        @Override
+        protected Void doInBackground() {
+        	System.out.println("Cheater fortifies");
+        	for (Country country : gameState.getCountries()) {
+        		if(country.getPlayer() == gameState.getCurrentPlayer()) {
+        			List<Country> neighbours = country.getNeighbours();
+        			for(Country neighbour : neighbours) {
+        				if(neighbour.getPlayer() != gameState.getCurrentPlayer()) {
+        					country.setArmy(country.getArmy() * 2);
+        					//System.out.println("Fortify: Armies doubled on " + country.getName());
+        				}
+        			}
+        		}
+        	}
+
+            pauseAndRefresh(gameState, PAUSE * 2);
+            return null;
+        }
+
+        /**
+         * Debug method
+         *
+         * @param chunks
+         */
+        @Override
+        protected void process(List<String> chunks) {
+            for (String c : chunks) {
+                System.out.println(c);
+            }
+        }
+
+        /**
+         * Automatic go to next turn when phase is done
+         */
+        @Override
+        protected void done() {
+            Game.getInstance().getGamePhaseStrategy().nextTurnButton(gameState);
+        }
     }
 }
