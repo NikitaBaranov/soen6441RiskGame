6aef274f5bbbe8b321032a87e3660a9f3fe0f035
Nikita Baranov <NikitaBaranov@gmail.com>
2018-11-29 00:43:22 -0500

    Tournament.

--- src/game/ui/Main.java
+++ src/game/ui/Main.java
@@ -124,7 +124,6 @@ public class Main {
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
-                        System.out.println("Working now");
                     }
                 }
             };
